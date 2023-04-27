80337afd79a5a3378b12c81c77c1b77f71025403
Rishabh <baidrishabh443@gmail.com>
2023-04-28 00:18:06 +0530

    voice handling

--- src/Components/Home/CreateForm.js
+++ src/Components/Home/CreateForm.js
@@ -1,80 +1,128 @@
-import React, { useState,useContext } from "react";
+import React, { useState, useContext } from "react";
 import Modal from "../Modal";
 import { SwitchesContext } from "../../context/switchContext";
-const fixedInputClass="rounded-md mt-2 mb-2 appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+const fixedInputClass =
+  "rounded-md mt-2 mb-2 appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm";
 
 const Initial = {
-  "name":"",
-  "logoName":"",
-  "nodeMCU":""
-}
+  name: "",
+  logoName: "",
+  nodeMCU: "",
+};
 let LIMIT = 1;
-export default function CreateForm({isOpen, handleClose}) {
-  const {switches, dispatch} = useContext(SwitchesContext)
+export default function CreateForm({ isOpen, handleClose }) {
+  const { switches, dispatch } = useContext(SwitchesContext);
   const [formOpen, setForm] = useState("switch");
-  const [state, setState] = useState(Initial)
+  const [state, setState] = useState(Initial);
 
   const handleChange = (e) => {
-    setState({...state, [e.target.name]:e.target.value})
-  }
+    setState({ ...state, [e.target.name]: e.target.value });
+  };
 
   const onSubmitSwitch = (e) => {
-    if(switches.length>=1)
-      return alert("Limit of creating switches is one for testing purposes!")
-    e.stopPropagation()
-    e.preventDefault()
+    if (switches.length >= 1)
+      return alert("Limit of creating switches is one for testing purposes!");
+    e.stopPropagation();
+    e.preventDefault();
     //console.log(state)
     dispatch({
-      type:"CREATE",
-      payload:state
-    })
-    setState(Initial)
-    handleClose()
-  }
+      type: "CREATE",
+      payload: state,
+    });
+    setState(Initial);
+    handleClose();
+  };
 
   const onSubmitNode = (e) => {
-    e.stopPropagation()
-    e.preventDefault()
-  }
+    e.stopPropagation();
+    e.preventDefault();
+  };
 
   return (
     <Modal isOpen={isOpen}>
       {formOpen !== "" ? (
         formOpen === "switch" ? (
           <>
-           <span className="cursor-pointer" onClick={()=> setForm("")}>back</span>
+            <span className="cursor-pointer" onClick={() => setForm("")}>
+              back
+            </span>
             <form onSubmit={onSubmitSwitch}>
-              <select name="nodeMCU" value={state.nodeMCU} onChange={handleChange} className={fixedInputClass} required>
+              <select
+                name="nodeMCU"
+                value={state.nodeMCU}
+                onChange={handleChange}
+                className={fixedInputClass}
+                required
+              >
                 <option>Please Select NodeMCU</option>
                 <option value={"N1"}>N1</option>
               </select>
 
-            
-              <input name="name" placeholder="Name" value={state.name} onChange={handleChange} className={fixedInputClass} required />
-              
-              <select name="logoName" value={state.logoName} onChange={handleChange} className={fixedInputClass} required>
+              <input
+                name="name"
+                placeholder="Name"
+                value={state.name}
+                onChange={handleChange}
+                className={fixedInputClass}
+                required
+              />
+
+              <select
+                name="logoName"
+                value={state.logoName}
+                onChange={handleChange}
+                className={fixedInputClass}
+                required
+              >
                 <option>Please Select Logo</option>
                 <option value={"fan"}>fan</option>
-                
               </select>
 
-              <button onClick={onSubmitSwitch} className="bg-blue-500 w-full hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              <button
+                onClick={onSubmitSwitch}
+                className="bg-blue-500 w-full hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              >
                 Create
               </button>
             </form>
           </>
         ) : (
           <>
-            <span className="cursor-pointer" onClick={()=> setForm("")}>back</span>
-            <form >
-              
-            
-              <input name="name" placeholder="Name" value={state.name} onChange={handleChange} className={fixedInputClass} required />
-              <input name="serialNo" placeholder="SerialNo" value={state.serialNo} onChange={handleChange} className={fixedInputClass} required />
-              <input name="switchNo" type={"number"} placeholder="Number of Switches" value={state.switchNo} onChange={handleChange} className={fixedInputClass} required />
-              
+            <span className="cursor-pointer" onClick={() => setForm("")}>
+              back
+            </span>
+            <form>
+              <input
+                name="name"
+                placeholder="Name"
+                value={state.name}
+                onChange={handleChange}
+                className={fixedInputClass}
+                required
+              />
+              <input
+                name="serialNo"
+                placeholder="SerialNo"
+                value={state.serialNo}
+                onChange={handleChange}
+                className={fixedInputClass}
+                required
+              />
+              <input
+                name="switchNo"
+                type={"number"}
+                placeholder="Number of Switches"
+                value={state.switchNo}
+                onChange={handleChange}
+                className={fixedInputClass}
+                required
+              />
 
-              <button onSubmit={onSubmitNode} type="submit" className="bg-blue-500 w-full hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              <button
+                onSubmit={onSubmitNode}
+                type="submit"
+                className="bg-blue-500 w-full hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              >
                 Create
               </button>
             </form>
@@ -82,13 +130,24 @@ export default function CreateForm({isOpen, handleClose}) {
         )
       ) : (
         <div className="flex flex-col bg-white relative">
-          <div onClick={handleClose} className="absolute -right-3 -top-5 flex justify-end text-xl cursor-pointer">
+          <div
+            onClick={handleClose}
+            className="absolute -right-3 -top-5 flex justify-end text-xl cursor-pointer"
+          >
             x
           </div>
-          <div onClick={()=>setForm("switch")} className="p-3 hover:bg-gray-200 rounded-md">
+          <div
+            onClick={() => setForm("switch")}
+            className="p-3 hover:bg-gray-200 rounded-md"
+          >
             Add Smart Switch
           </div>
-          <div onClick={()=>setForm("mcu")} className="p-3 hover:bg-gray-200 rounded-md">Add NodeMCU</div>
+          <div
+            onClick={() => setForm("mcu")}
+            className="p-3 hover:bg-gray-200 rounded-md"
+          >
+            Add NodeMCU
+          </div>
         </div>
       )}
     </Modal>
--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,43 +1,120 @@
-import React, { useContext, useState } from 'react';
-import { Navbar } from '../../Components/Navbar';
-import Modal from '../../Components/Modal';
-import CreateForm from '../../Components/Home/CreateForm';
-import SmartSwitch from '../../Components/SmartSwitch';
-import { SwitchesContext } from '../../context/switchContext';
-import Sensor from '../../Components/Sensor';
+import React, { useContext, useState, useRef, useEffect } from "react";
+import { Navbar } from "../../Components/Navbar";
+import Modal from "../../Components/Modal";
+import CreateForm from "../../Components/Home/CreateForm";
+import SmartSwitch from "../../Components/SmartSwitch";
+import { SwitchesContext } from "../../context/switchContext";
+import Sensor from "../../Components/Sensor";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+
+let svgIcons = {
+  "light":{
+    "microphone":<svg fill="#ffffff" width="24" height="24" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg">
+    <title>microphone</title>
+    <path d="M4 20q0 3.264 1.6 6.048t4.384 4.352 6.016 1.6 6.016-1.6 4.384-4.352 1.6-6.048q0-0.832-0.576-1.408t-1.408-0.576-1.44 0.576-0.576 1.408q0 3.328-2.336 5.664t-5.664 2.336-5.664-2.336-2.336-5.664q0-0.832-0.576-1.408t-1.408-0.576-1.44 0.576-0.576 1.408zM10.016 20q0 2.496 1.728 4.256t4.256 1.76 4.256-1.76 1.76-4.256v-1.984h-3.008q-0.416 0-0.704-0.288t-0.288-0.704 0.288-0.704 0.704-0.32h3.008v-1.984h-3.008q-0.416 0-0.704-0.288t-0.288-0.704 0.288-0.704 0.704-0.32h3.008v-1.984h-3.008q-0.416 0-0.704-0.288t-0.288-0.704 0.288-0.704 0.704-0.32h3.008v-1.984h-3.008q-0.416 0-0.704-0.288t-0.288-0.704 0.288-0.704 0.704-0.32h2.624q-0.608-1.76-2.144-2.88t-3.488-1.12q-1.92 0-3.456 1.12t-2.176 2.88h2.624q0.416 0 0.704 0.32t0.32 0.704-0.32 0.704-0.704 0.288h-2.976v1.984h2.976q0.416 0 0.704 0.32t0.32 0.704-0.32 0.704-0.704 0.288h-2.976v1.984h2.976q0.416 0 0.704 0.32t0.32 0.704-0.32 0.704-0.704 0.288h-2.976v1.984h2.976q0.416 0 0.704 0.32t0.32 0.704-0.32 0.704-0.704 0.288h-2.976v1.984z"></path>
+    </svg>
+  },
+  "dark":{
+    "microphone":<svg fill="gray" width="24" height="24" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg">
+    <title>microphone</title>
+    <path d="M4 20q0 3.264 1.6 6.048t4.384 4.352 6.016 1.6 6.016-1.6 4.384-4.352 1.6-6.048q0-0.832-0.576-1.408t-1.408-0.576-1.44 0.576-0.576 1.408q0 3.328-2.336 5.664t-5.664 2.336-5.664-2.336-2.336-5.664q0-0.832-0.576-1.408t-1.408-0.576-1.44 0.576-0.576 1.408zM10.016 20q0 2.496 1.728 4.256t4.256 1.76 4.256-1.76 1.76-4.256v-1.984h-3.008q-0.416 0-0.704-0.288t-0.288-0.704 0.288-0.704 0.704-0.32h3.008v-1.984h-3.008q-0.416 0-0.704-0.288t-0.288-0.704 0.288-0.704 0.704-0.32h3.008v-1.984h-3.008q-0.416 0-0.704-0.288t-0.288-0.704 0.288-0.704 0.704-0.32h3.008v-1.984h-3.008q-0.416 0-0.704-0.288t-0.288-0.704 0.288-0.704 0.704-0.32h2.624q-0.608-1.76-2.144-2.88t-3.488-1.12q-1.92 0-3.456 1.12t-2.176 2.88h2.624q0.416 0 0.704 0.32t0.32 0.704-0.32 0.704-0.704 0.288h-2.976v1.984h2.976q0.416 0 0.704 0.32t0.32 0.704-0.32 0.704-0.704 0.288h-2.976v1.984h2.976q0.416 0 0.704 0.32t0.32 0.704-0.32 0.704-0.704 0.288h-2.976v1.984h2.976q0.416 0 0.704 0.32t0.32 0.704-0.32 0.704-0.704 0.288h-2.976v1.984z"></path>
+    </svg>
+  }
+}
 
 const Home = () => {
-  const [isOpen, setOpen] = useState(false)
-   const {switches, dispatch} = useContext(SwitchesContext)
-   const handleSwitchClick = (name, prevStatus) => { 
+  const commands = [
+    {
+      command: "turn *",
+      callback: (sw) => {
+        
+        if(sw.toLowerCase().trim().startsWith("on")||sw.toLowerCase().trim().startsWith("off")){
+          dispatch({
+            type: "UPDATE",
+            payload: {
+              name: switches[0]?.name,
+              status: sw.toLowerCase().trim().startsWith("on") ? 1 : 0,
+            },
+          });
+        }else{
+          console.log("Say turn on")
+        }
+      },
+    },
+  ];
+  const { transcript, resetTranscript } = useSpeechRecognition({ commands });
+  const [isOpen, setOpen] = useState(false);
+  const { switches, dispatch } = useContext(SwitchesContext);
+  const [ isListening ,setIsListening] = useState(false)
+  const handleSwitchClick = (name, prevStatus) => {
     dispatch({
-      type:"UPDATE",
-      payload:{
+      type: "UPDATE",
+      payload: {
         name: name,
-        status: prevStatus?0:1
-      }
-    })
-   }
-   
+        status: prevStatus ? 0 : 1,
+      },
+    });
+  };
+
+  const handleListing = () => {
+    localStorage.setItem("voice","on")
+    SpeechRecognition.startListening({
+      continuous: true
+    });
+    setIsListening(true);
+  };
+
+  const stopHandle = () => {
+    setIsListening(false);
+    SpeechRecognition.stopListening();
+  };
+
+  const handleReset = () => {
+    localStorage.setItem("voice","off")
+    stopHandle();
+    resetTranscript();
+  };
+
+  useEffect(()=>{
+    if(localStorage.getItem('voice')=="on"){
+      handleListing()
+      setIsListening(true)
+    }
+  },[])
+
+ 
   return (
-    <div className='flex flex-col'>
-        {/* <Navbar
+    <div className="flex flex-col">
+      {/* <Navbar
           handleAdd={()=>setOpen(!isOpen)}
         /> */}
-        <CreateForm
-          isOpen={isOpen}
-          handleClose={()=>setOpen(!isOpen)}
-        />
-      <div className='flex flex-row p-4'>
-       { switches.map((obj)=><SmartSwitch 
-          logoName={"fan"}
-          name={obj.name}
-          mcuName={"N1"}
-          status={obj.status}
-          handleClick={handleSwitchClick}
-        />)}
+      <CreateForm isOpen={isOpen} handleClose={() => setOpen(!isOpen)} />
+      <div className="flex flex-row p-4">
+        {switches.map((obj) => (
+          <SmartSwitch
+            logoName={"fan"}
+            name={obj.name}
+            mcuName={"N1"}
+            status={obj.status}
+            handleClick={handleSwitchClick}
+          />
+        ))}
       </div>
-      <Sensor/>
+      <Sensor />
+      <div
+        className={`w-12 h-12 ${isListening?"bg-blue-500":"bg-gray-200"} rounded-full p-2 shadow-xl flex flex-col flex justify-center items-center fixed bottom-12 right-4`}
+        onClick={()=>{
+          if(isListening)
+            handleReset()
+          else
+            handleListing()
+        }}
+      >
+        {svgIcons[isListening?'light':'dark']['microphone']}
+      </div>
+      
     </div>
   );
 };
